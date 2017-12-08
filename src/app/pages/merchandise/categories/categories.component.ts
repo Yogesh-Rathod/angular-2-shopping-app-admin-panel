a12bb40d16eba2cf53fb4be74eea055df0e3b421
Yogesh Rathod <yogesh.rathod@loylty.in>
2017-12-08 15:10:58 +0530

    HMAC Encryption Sample Code

--- src/app/pages/merchandise/categories/categories.component.ts
+++ src/app/pages/merchandise/categories/categories.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AES } from "crypto-js/aes";
+import * as CryptoJS from "crypto-js";
+import * as utf8 from 'utf8';
+
 import * as _ from 'lodash';
 declare let $: any;
 
@@ -33,7 +37,7 @@ export class CategoriesComponent implements OnInit {
       $('[data-toggle="tooltip"]').tooltip();
     });
     this.getAllCategories();
-    // this.bulkUpload();
+    this.createHMACSignature();
   }
 
   getAllCategories() {
@@ -63,6 +67,33 @@ export class CategoriesComponent implements OnInit {
 
   bulkUpload() {
     const activeModal = this.modalService.open( BulkUploadComponent, { size: 'sm' } );
+  }
+
+  createHMACSignature() {
+    let requestUrl = encodeURI('http://localhost:3000/'),
+        timestamp = + new Date(),
+        nounce = btoa(CryptoJS.lib.WordArray.random(8)),
+        signatureRaw = `clientIdget${requestUrl}${timestamp}${nounce}`;
+
+    let content = {
+      username: 'Yogesh',
+      password: 'Password'
+    };
+    let contentString = '';
+
+    if (content) {
+      contentString = JSON.stringify(content);
+      let updatedContent = btoa( CryptoJS.SHA256(  CryptoJS.MD5( utf8.encode(contentString)  )  ) );
+      signatureRaw = signatureRaw + updatedContent;
+    }
+
+    const clientSecret = utf8.encode('clientSecret');
+    signatureRaw = utf8.encode(signatureRaw);
+
+    const finalSignature = btoa(CryptoJS.HmacSHA256(signatureRaw, clientSecret) );
+
+    console.log("finalSignature ", finalSignature);
+    return `clientId:${finalSignature}:${nounce}:${timestamp}`;
   }
 
 

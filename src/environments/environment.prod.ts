87e0d5956089acaf6a3a433d5552cb64885c61e4
Yogesh Rathod <yogesh.rathod@loylty.in>
2018-01-10 19:11:39 +0530

    Changes in ops products

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-    production: false,
-    moviesApiUrl: 'http://192.168.61.202/LRAS.MovieAPI/Support/',
+    production: true,
+    moviesApiUrl: 'http://192.168.61.203/LRAS.MovieAPI/Support/',
     apiUrl: 'http://192.168.250.224:8080',
     crmUrl: 'http://192.168.250.222:9081/crm',
     loginUrl: 'http://localhost:4300/',

8900114535b7770dacfbd0ceb38480ac883e4fed
Yogesh Rathod <yogesh.rathod@loylty.in>
2018-01-10 15:32:30 +0530

    New Service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -45,7 +45,7 @@ export class ProductsService {
         const url = `${environment.merchandiseUrl}Seller/Product`;
         this.headers.set('Authorization', this.commonAppSer.crateAuthorization());
         // this.headers.set('LRSignAuth', this.commonAppSer.createHMACSignature('PUT', url, product));
-        return this.http.put(url, JSON.stringify(product), this.options)
+        return this.http.post(url, JSON.stringify(product), this.options)
             .timeout(environment.timeOut)
             .toPromise()
             .then(this.responseHandler.handleResponse)

d74de2f8f587594ef7613013b3286389d4717432
sachink <sachin.kurkute@loylty.in>
2018-01-15 21:35:54 +0530

    Fixed bulk upload issue

--- src/app/pages/merchandise/orders/shipped/shipped.component.ts
+++ src/app/pages/merchandise/orders/shipped/shipped.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Output, Input, EventEmitter, OnChanges } from '@angu
 import { ProductsService, OrdersService, JsonToExcelService } from 'app/services';
 import * as _ from 'lodash';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { SellerOrdersBulkUploadComponent } from '../bulk-upload/bulk-upload.component';
+import { SellerOrdersAdminBulkUploadComponent } from '../bulk-upload/bulk-upload.component';
 
 @Component({
     selector: 'app-shipped',
@@ -37,7 +37,7 @@ export class ShippedComponent implements OnInit {
     }
 
     importOrders() {
-        const activeModal = this.modalService.open(SellerOrdersBulkUploadComponent, { size: 'sm' });
+        const activeModal = this.modalService.open(SellerOrdersAdminBulkUploadComponent, { size: 'sm' });
         activeModal.componentInstance.fileUrl = 'DispatchedToDelivered.xlsx';
         activeModal.componentInstance.request = 'shipped';
         activeModal.result.then(status => {

ff1cd4d5ef6c2df1ccf00095e7d1d875efbae9a8
Yogesh Rathod <yogesh.rathod@loylty.in>
2018-02-01 12:55:18 +0530

    QA API URL IN ENV FILE

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -5,11 +5,11 @@
 
 export const environment = {
     production: true,
-    moviesApiUrl: 'http://192.168.32.54/V1/Support/',
+    moviesApiUrl: 'http://192.168.61.203/LRAS.MovieAPI/Support/',
     crmUrl: 'http://192.168.250.222:9081/crm',
-    citiesAPIUrl: 'http://192.168.32.100/LRAS.CommonAPI/V1/GetCities',
-    rbacUrl: 'http://192.168.32.100/LRAS.Brix.InternalUserManagementAPI/V1/',
-    merchandiseUrl: 'http://192.168.32.101/LRAS.MerchandiseSupportAPI/',
+    citiesAPIUrl: 'http://192.168.61.203/LRAS.CommonAPI/GetCities',
+    rbacUrl: 'http://192.168.61.203/LRAS.Brix.InternalUserManagementAPI/',
+    merchandiseUrl: 'http://192.168.61.203/LRAS.MerchandiseSupportAPI/',
     appName: 'MERCHANDISE',
     hmacCliendId: 'lvbportal',
     hmacClientSecret: 'secret',

acc37a34f5ca3b843acf5a0fe2b67794fc41f1a7
Yogesh Rathod <yogesh.rathod@loylty.in>
2018-02-01 12:15:26 +0530

    My tasks only visible to Admin Useers

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -6,6 +6,8 @@ import { AppState } from 'app/app.service';
 import { UserService } from 'lrshared_modules/pages/user-management/user.service';
 import { CookieService } from 'ngx-cookie';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'pages',
   templateUrl: './pages.component.html'
@@ -44,6 +46,7 @@ export class Pages {
                         return role.RoleName;
                     });
                 }
+
                 this._cookieService.put('userRoles', userRoles);
                 var MenuListArray = userMenus.map(function(item) {
                     return item['MenuCode'];
@@ -58,7 +61,18 @@ export class Pages {
                 this._cookieService.put('sellerId', res.Data.Id);
                 this._cookieService.put('MenuListAllowed', MenuListArray);
                 var PAGES_MENU_NEW = JSON.parse(JSON.stringify(PAGES_MENU));
-                PAGES_MENU_NEW[0].children = customMenu;
+                let updatedMenuItems = JSON.parse(JSON.stringify(customMenu));;
+                _.each(updatedMenuItems, (item) => {
+                    if (item.MenuCode === 'PRD') {
+                        if (!(userRoles.indexOf('SuperAdmin') >= 0 || userRoles.indexOf('Admin') >= 0)) {
+                            if (item['children'][4] && item['children'][4].path === 'approvals') {
+                                item['children'].pop();
+                                return;
+                            }
+                        }
+                    }
+                });
+                PAGES_MENU_NEW[0].children = updatedMenuItems;
                 this._menuService.updateMenuByRoutes(<Routes>PAGES_MENU_NEW);
             }).catch(rej => {
                 console.log("Error: ",rej);

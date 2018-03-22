fb48d269e267312ef1ef5d0920fd2fa75e97284d
Yogesh Rathod <yogesh.rathod@loylty.in>
2018-03-22 16:20:11 +0530

    Code Indentation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,28 +3,28 @@ import { Component, OnInit } from '@angular/core';
 import { AppStateManagementService } from 'users_modules/services';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  userInfo: any;
+    userInfo: any;
 
-  constructor(
-    private appStateManagementService: AppStateManagementService
-  ) {
-    this.appStateManagementService.retrieveAppStateCK('MERCHANDISE.userData').
-      then((userInfo) => {
-        this.userInfo = JSON.parse(userInfo);
-      }).catch((error) => {
-        this.userInfo = {
-          username: 'Unknown User'
-        }
-      });
-  }
+    constructor(
+        private appStateManagementService: AppStateManagementService
+    ) {
+        this.appStateManagementService.retrieveAppStateCK('MERCHANDISE.userData').
+            then((userInfo) => {
+                this.userInfo = JSON.parse(userInfo);
+            }).catch((error) => {
+                this.userInfo = {
+                    username: 'Unknown User'
+                }
+            });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }

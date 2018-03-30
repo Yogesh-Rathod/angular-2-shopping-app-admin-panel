6b07c0c618e742e48f7bf08a732b454cca959930
Yogesh Rathod <yogesh.rathod@loylty.in>
2018-03-30 13:09:54 +0530

    Remember Me Password option

--- src/users_modules/pages/login/login.component.ts
+++ src/users_modules/pages/login/login.component.ts
@@ -18,170 +18,198 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { MerchandiseService } from 'users_modules/services/roleServices';
 import { BaThemeSpinner } from 'app/theme/services';
+import { AppStateManagementService } from 'users_modules/services';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
+    loginForm: FormGroup;
 
-  isError = false;
-  errorMessage: string;
-  isHidden = true;
-  isHiddenForgot = true;
-  role: any = [];
-  forgotPassword = false;
-  forgotPasswordForm: FormGroup;
-  email: AbstractControl;
-  password: AbstractControl;
-  forgotPasswordEmail: AbstractControl;
+    isError = false;
+    errorMessage: string;
+    isHidden = true;
+    isHiddenForgot = true;
+    role: any = [];
+    forgotPassword = false;
+    forgotPasswordForm: FormGroup;
+    email: AbstractControl;
+    password: AbstractControl;
+    forgotPasswordEmail: AbstractControl;
+    rememberMe: AbstractControl;
 
-  constructor(
-    private _cookieService: CookieService,
-    private fb: FormBuilder,
-    private router: Router,
-    private loginService: LoginService,
-    private toastr: ToastsManager,
-    private vcr: ViewContainerRef,
-    private _state: GlobalState,
-    private activeModal: NgbActiveModal,
-    private merchandiseService: MerchandiseService,
-    private _spinner: BaThemeSpinner,
-  ) {
+    constructor(
+        private appStateManagementService: AppStateManagementService,
+        private _cookieService: CookieService,
+        private fb: FormBuilder,
+        private router: Router,
+        private loginService: LoginService,
+        private toastr: ToastsManager,
+        private vcr: ViewContainerRef,
+        private _state: GlobalState,
+        private activeModal: NgbActiveModal,
+        private merchandiseService: MerchandiseService,
+        private _spinner: BaThemeSpinner,
+    ) {
 
-  }
+    }
 
-  ngOnInit() {
-    this.createForms();
-  }
+    ngOnInit() {
+        this.createForms();
+        this.setLoginFormValues();
+    }
 
-  createForms() {
-    this.loginForm = this.fb.group({
-      'email': ['', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$|^[a-zA-Z0-9.-_]*$')
-      ])],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(5)])],
-    });
+    setLoginFormValues() {
+        this.appStateManagementService.retrieveAppStateLS('userCredentials').
+            then((userCredential) => {
+                if (userCredential) {
+                    const userCred = JSON.parse(userCredential);
+                    console.log("userCred ", userCred);
+                    this.loginForm.controls['email'].patchValue(userCred.username);
+                    this.loginForm.controls['password'].patchValue(userCred.password);
+                    this.loginForm.controls['rememberMe'].patchValue('true');
+                }
+            });
+    }
 
-    this.email = this.loginForm.controls['email'];
-    this.password = this.loginForm.controls['password'];
+    createForms() {
+        this.loginForm = this.fb.group({
+            'email': ['', Validators.compose([
+                Validators.required,
+                Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$|^[a-zA-Z0-9.-_]*$')
+            ])],
+            'password': ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+            'rememberMe': ['']
+        });
 
-    this.forgotPasswordForm = this.fb.group({
-      // tslint:disable-next-line:max-line-length
-      'forgotPasswordEmail': ['', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$|^[a-zA-Z0-9.-_]*$')
-      ])],
-    });
+        this.email = this.loginForm.controls['email'];
+        this.password = this.loginForm.controls['password'];
+        this.rememberMe = this.loginForm.controls['rememberMe'];
 
-    this.forgotPasswordEmail = this.forgotPasswordForm.controls['forgotPasswordEmail'];
-  }
+        this.forgotPasswordForm = this.fb.group({
+            // tslint:disable-next-line:max-line-length
+            'forgotPasswordEmail': ['', Validators.compose([
+                Validators.required,
+                Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$|^[a-zA-Z0-9.-_]*$')
+            ])],
+        });
 
-  onForgotFormSubmit(value) {
-    if (this.forgotPasswordForm.valid && this.isHiddenForgot) {
-      this.isHiddenForgot = false;
-      this.loginService.userResetPassword(value.forgotPasswordEmail)
-        .then((res) => {
-          this.isHiddenForgot = true;
-          this.forgotPassword = false;
-          this.forgotPasswordForm.reset();
-          this.toastr.success('A new password has been sent to your e-mail address.');
-        })
-        .catch(rej => {
-          this.isHiddenForgot = true;
-          this.errorMessage = rej.message ? rej.message : 'Sorry, something went wrong. Please try again later.';
-          this.toastr.error(this.errorMessage);
+        this.forgotPasswordEmail = this.forgotPasswordForm.controls['forgotPasswordEmail'];
+    }
+
+    onForgotFormSubmit(value) {
+        if (this.forgotPasswordForm.valid && this.isHiddenForgot) {
+            this.isHiddenForgot = false;
+            this.loginService.userResetPassword(value.forgotPasswordEmail)
+                .then((res) => {
+                    this.isHiddenForgot = true;
+                    this.forgotPassword = false;
+                    this.forgotPasswordForm.reset();
+                    this.toastr.success('A new password has been sent to your e-mail address.');
+                })
+                .catch(rej => {
+                    this.isHiddenForgot = true;
+                    this.errorMessage = rej.message ? rej.message : 'Sorry, something went wrong. Please try again later.';
+                    this.toastr.error(this.errorMessage);
+                });
+        }
+    }
+
+    onSubmit(loginForm) {
+        if (loginForm.value.rememberMe) {
+            const userCredentials = {
+                username: loginForm.value.email,
+                password: loginForm.value.password
+            };
+            this.appStateManagementService.updateAppStateLS('userCredentials', JSON.stringify(userCredentials));
+        } else {
+            this.appStateManagementService.clearAppStateLSSingle('userCredentials');
+        }
+        try {
+            if (loginForm.valid && this.isHidden) {
+                this.isHidden = false;
+
+                this.loginService.userLogin({ UserName: loginForm.value.email, Password: loginForm.value.password })
+                    .then((res) => {
+                        this.isHidden = true;
+                        if (res.Code === 500) {
+                            this.toastr.error('Login failed! Please recheck username & password.');
+                            return;
+                        }
+                        const loginPayload = {
+                            accessToken: res.Data.Accesstoken,
+                            refreshToken: res.Data.RefreshToken
+                        };
+                        this.setCookieData(loginPayload, environment.appName);
+                    })
+                    .catch(rej => {
+                        this.isHidden = true;
+                        this.errorMessage = rej.message ? rej.message : 'Sorry, something went wrong. Please try again later.';
+                        this.toastr.error(this.errorMessage);
+                    });
+            }
+        } catch (ex) {
+        }
+    }
+
+    setCookie(key: string, value: string) {
+        return this._cookieService.put(key, value, {
+            domain: environment.domainName
         });
     }
-  }
 
-  onSubmit(loginForm) {
-    try {
-      if (loginForm.valid && this.isHidden) {
-        this.isHidden = false;
+    setCookieData(loginPayload, appName) {
+        this.role.push(loginPayload);
 
-        this.loginService.userLogin({ UserName: loginForm.value.email, Password: loginForm.value.password })
-          .then((res) => {
-            this.isHidden = true;
-            if (res.Code === 500) {
-              this.toastr.error('Login failed! Please recheck username & password.');
-              return;
-            }
-            const loginPayload = {
-              accessToken: res.Data.Accesstoken,
-              refreshToken: res.Data.RefreshToken
-            };
-            this.setCookieData(loginPayload, environment.appName);
-          })
-          .catch(rej => {
-            this.isHidden = true;
-            this.errorMessage = rej.message ? rej.message : 'Sorry, something went wrong. Please try again later.';
-            this.toastr.error(this.errorMessage);
-          });
-      }
-    } catch (ex) {
+        this._state.notifyDataChanged('loginSetCookie', true);
+
+        this._cookieService.put(`${environment.appName}.token`, JSON.stringify(loginPayload));
+
+        this._cookieService.put(`${environment.appName}.userData`, JSON.stringify({
+            username: loginPayload.username || this.loginForm.value.email.toLowerCase(),
+        }));
+
+        switch (environment.appName) {
+
+            case 'MERCHANDISE':
+                this.merchandiseService.init()
+                    .then(() => {
+                        this.navigateToDashboard();
+                    })
+                    .catch(() => {
+                        this.somethingWentWrong();
+                    });
+                break;
+
+            default:
+                this.somethingWentWrong();
+                break;
+        }
     }
-  }
 
-  setCookie(key: string, value: string) {
-    return this._cookieService.put(key, value, {
-      domain: environment.domainName
-    });
-  }
-
-  setCookieData(loginPayload, appName) {
-    this.role.push(loginPayload);
-
-    this._state.notifyDataChanged('loginSetCookie', true);
-
-    this._cookieService.put(`${environment.appName}.token`, JSON.stringify(loginPayload));
-
-    this._cookieService.put(`${environment.appName}.userData`, JSON.stringify({
-      username: loginPayload.username || this.loginForm.value.email.toLowerCase(),
-    }));
-
-    switch (environment.appName) {
-
-      case 'MERCHANDISE':
-        this.merchandiseService.init()
-          .then(() => {
-            this.navigateToDashboard();
-          })
-          .catch(() => {
-            this.somethingWentWrong();
-          });
-        break;
-
-      default:
-        this.somethingWentWrong();
-        break;
-    }
-  }
-
-  somethingWentWrong() {
-    this.isHidden = true;
-    this._spinner.hide();
-    this.toastr.error('Something went wrong please try again.');
-  }
-
-  navigateToDashboard() {
-    this.router.navigate(['/']).then(() => {
-      this.isHidden = true;
-      this._spinner.hide();
-      setTimeout(() => {
-        this._state.notifyDataChanged('loginSetCookie', false);
-      }, 1500);
-    })
-      .catch((ex) => {
+    somethingWentWrong() {
         this.isHidden = true;
-      });
-  }
+        this._spinner.hide();
+        this.toastr.error('Something went wrong please try again.');
+    }
 
-  backToLogin() {
-    this.forgotPasswordForm.reset();
-    this.forgotPassword = false;
-  }
+    navigateToDashboard() {
+        this.router.navigate(['/']).then(() => {
+            this.isHidden = true;
+            this._spinner.hide();
+            setTimeout(() => {
+                this._state.notifyDataChanged('loginSetCookie', false);
+            }, 1500);
+        })
+            .catch((ex) => {
+                this.isHidden = true;
+            });
+    }
+
+    backToLogin() {
+        this.forgotPasswordForm.reset();
+        this.forgotPassword = false;
+    }
 }
--- src/users_modules/services/app-state-management.service.ts
+++ src/users_modules/services/app-state-management.service.ts
@@ -6,214 +6,218 @@ import { isUndefined } from 'util';
 @Injectable()
 export class AppStateManagementService {
 
-  constructor(
-    private cookieService: CookieService
-  ) { }
+    constructor(
+        private cookieService: CookieService
+    ) { }
 
-  private appStateManagementLSObservables = {};
-  private appStateManagementCKObservables = {};
+    private appStateManagementLSObservables = {};
+    private appStateManagementCKObservables = {};
 
-  // LOCAL STORAGE
+    // LOCAL STORAGE
 
-  private getValueFromLocalStorage(key: string): Promise<string> {
-    return new Promise(
-      (resolve, reject) => {
-        try {
-          resolve(localStorage.getItem(key));
-        } catch (reason) {
-          reject(reason);
-        }
-      }
-    );
-  }
-
-  private setValueToLocalStorage(key: string, value: string): Promise<string> {
-    return new Promise(
-      (resolve, reject) => {
-        try {
-          localStorage.setItem(key, value);
-          if (localStorage.getItem(key) === value) {
-            resolve(value);
-          }
-        } catch (reason) {
-          reject(reason);
-        }
-      }
-    );
-  }
-
-  updateAppStateLS(key: string, value: string): Promise<string> {
-    return this.setValueToLocalStorage(key, value);
-  }
-
-  retrieveAppStateLS(key: string): Promise<string> {
-    return this.getValueFromLocalStorage(key);
-  }
-
-  retrieveAppStateLS$(key: string): Observable<string> {
-    if (this.appStateManagementLSObservables[key]) {
-      return this.appStateManagementLSObservables[key];
-
-    } else {
-      const observable = new Observable(
-        subscriber => {
-          let value;
-          setInterval(
-            () => {
-              try {
-                this.getValueFromLocalStorage(key)
-                  .then(
-                    valueRetrieved => {
-                      if (value !== valueRetrieved) {
-                        subscriber.next(valueRetrieved);
-                        value = valueRetrieved;
-                      }
-                    }
-                  // }
-                  )
-                  .catch(
-                  reason => subscriber.error(reason)
-                  );
-              } catch (reason) {
-                subscriber.error(reason);
-              }
-            },
-            500
-          );
-        }
-      );
-
-      this.appStateManagementLSObservables[key] = observable;
-
-      return observable;
+    private getValueFromLocalStorage(key: string): Promise<string> {
+        return new Promise(
+            (resolve, reject) => {
+                try {
+                    resolve(localStorage.getItem(key));
+                } catch (reason) {
+                    reject(reason);
+                }
+            }
+        );
     }
-  }
 
-  clearAppStateLS(): Promise<boolean> {
-    return new Promise(
-      (resolve, reject) => {
-        try {
-
-          localStorage.clear();
-          setTimeout(
-            () => {
-              if (Object.keys(localStorage).length === 0) {
-                resolve(true);
-              } else {
-                resolve(false);
-              }
-            },
-            1000);
-        } catch (reason) {
-          reject(reason);
-        }
-      }
-    );
-  }
-
-
-  // COOKIES
-
-  private getValueFromCookie(key: string): Promise<string> {
-    return new Promise(
-      (resolve, reject) => {
-        try {
-          const currentValue = this.cookieService.get(key) ? this.cookieService.get(key) : null;
-          resolve(currentValue);
-        } catch (reason) {
-          reject(reject);
-        }
-      }
-    );
-  }
-
-  private setValueToCookie(key: string, value: string): Promise<string> {
-    return new Promise(
-      (resolve, reject) => {
-        try {
-          this.cookieService.put(key, value);
-          if (this.cookieService.get(key) === value) {
-            resolve(value);
-          }
-        } catch (reason) {
-          reject(reason);
-        }
-      }
-    );
-  }
-
-  updateAppStateCK(key: string, value: string): Promise<string> {
-    return this.setValueToCookie(key, value);
-  }
-
-  retrieveAppStateCK(key: string): Promise<string> {
-    return this.getValueFromCookie(key);
-  }
-
-  retrieveAppStateCK$(key: string): Observable<string> {
-    if (this.appStateManagementCKObservables[key]) {
-      return this.appStateManagementCKObservables[key];
-
-    } else {
-      const observable = new Observable(
-        subscriber => {
-          let value;
-          setInterval(
-            () => {
-              try {
-                this.getValueFromCookie(key)
-                  .then(
-                    valueRetrieved => {
-                      // collapse all undefined to null in case of cookies
-
-                      valueRetrieved = isUndefined(valueRetrieved) ? null : valueRetrieved;
-
-                      if (value !== valueRetrieved) {
-                        subscriber.next(valueRetrieved);
-                        value = valueRetrieved;
-                      }
+    private setValueToLocalStorage(key: string, value: string): Promise<string> {
+        return new Promise(
+            (resolve, reject) => {
+                try {
+                    localStorage.setItem(key, value);
+                    if (localStorage.getItem(key) === value) {
+                        resolve(value);
                     }
-                  // }
-                  )
-                  .catch(
-                  reason => subscriber.error(reason)
-                  );
-              } catch (reason) {
-                subscriber.error(reason);
-              }
-            },
-            500
-          );
-        }
-      );
-
-      this.appStateManagementCKObservables[key] = observable;
-
-      return observable;
+                } catch (reason) {
+                    reject(reason);
+                }
+            }
+        );
     }
-  }
 
-  clearAppStateCK(): Promise<boolean> {
-    return new Promise(
-      (resolve, reject) => {
-        try {
-          this.cookieService.removeAll();
+    updateAppStateLS(key: string, value: string): Promise<string> {
+        return this.setValueToLocalStorage(key, value);
+    }
 
-          setTimeout(
-            () => {
-              if (Object.keys(this.cookieService.getAll()).length === 0) {
-                resolve(true);
-              } else {
-                resolve(false);
-              }
-            },
-            1000);
-        } catch (reason) {
-          reject(reason);
+    retrieveAppStateLS(key: string): Promise<string> {
+        return this.getValueFromLocalStorage(key);
+    }
+
+    retrieveAppStateLS$(key: string): Observable<string> {
+        if (this.appStateManagementLSObservables[key]) {
+            return this.appStateManagementLSObservables[key];
+
+        } else {
+            const observable = new Observable(
+                subscriber => {
+                    let value;
+                    setInterval(
+                        () => {
+                            try {
+                                this.getValueFromLocalStorage(key)
+                                    .then(
+                                        valueRetrieved => {
+                                            if (value !== valueRetrieved) {
+                                                subscriber.next(valueRetrieved);
+                                                value = valueRetrieved;
+                                            }
+                                        }
+                                        // }
+                                    )
+                                    .catch(
+                                        reason => subscriber.error(reason)
+                                    );
+                            } catch (reason) {
+                                subscriber.error(reason);
+                            }
+                        },
+                        500
+                    );
+                }
+            );
+
+            this.appStateManagementLSObservables[key] = observable;
+
+            return observable;
         }
-      }
-    );
-  }
+    }
+
+    clearAppStateLS(): Promise<boolean> {
+        return new Promise(
+            (resolve, reject) => {
+                try {
+
+                    localStorage.clear();
+                    setTimeout(
+                        () => {
+                            if (Object.keys(localStorage).length === 0) {
+                                resolve(true);
+                            } else {
+                                resolve(false);
+                            }
+                        },
+                        1000);
+                } catch (reason) {
+                    reject(reason);
+                }
+            }
+        );
+    }
+
+    clearAppStateLSSingle(key) {
+        localStorage.removeItem(key);
+    }
+
+
+    // COOKIES
+
+    private getValueFromCookie(key: string): Promise<string> {
+        return new Promise(
+            (resolve, reject) => {
+                try {
+                    const currentValue = this.cookieService.get(key) ? this.cookieService.get(key) : null;
+                    resolve(currentValue);
+                } catch (reason) {
+                    reject(reject);
+                }
+            }
+        );
+    }
+
+    private setValueToCookie(key: string, value: string): Promise<string> {
+        return new Promise(
+            (resolve, reject) => {
+                try {
+                    this.cookieService.put(key, value);
+                    if (this.cookieService.get(key) === value) {
+                        resolve(value);
+                    }
+                } catch (reason) {
+                    reject(reason);
+                }
+            }
+        );
+    }
+
+    updateAppStateCK(key: string, value: string): Promise<string> {
+        return this.setValueToCookie(key, value);
+    }
+
+    retrieveAppStateCK(key: string): Promise<string> {
+        return this.getValueFromCookie(key);
+    }
+
+    retrieveAppStateCK$(key: string): Observable<string> {
+        if (this.appStateManagementCKObservables[key]) {
+            return this.appStateManagementCKObservables[key];
+
+        } else {
+            const observable = new Observable(
+                subscriber => {
+                    let value;
+                    setInterval(
+                        () => {
+                            try {
+                                this.getValueFromCookie(key)
+                                    .then(
+                                        valueRetrieved => {
+                                            // collapse all undefined to null in case of cookies
+
+                                            valueRetrieved = isUndefined(valueRetrieved) ? null : valueRetrieved;
+
+                                            if (value !== valueRetrieved) {
+                                                subscriber.next(valueRetrieved);
+                                                value = valueRetrieved;
+                                            }
+                                        }
+                                        // }
+                                    )
+                                    .catch(
+                                        reason => subscriber.error(reason)
+                                    );
+                            } catch (reason) {
+                                subscriber.error(reason);
+                            }
+                        },
+                        500
+                    );
+                }
+            );
+
+            this.appStateManagementCKObservables[key] = observable;
+
+            return observable;
+        }
+    }
+
+    clearAppStateCK(): Promise<boolean> {
+        return new Promise(
+            (resolve, reject) => {
+                try {
+                    this.cookieService.removeAll();
+
+                    setTimeout(
+                        () => {
+                            if (Object.keys(this.cookieService.getAll()).length === 0) {
+                                resolve(true);
+                            } else {
+                                resolve(false);
+                            }
+                        },
+                        1000);
+                } catch (reason) {
+                    reject(reason);
+                }
+            }
+        );
+    }
 
 
 }
